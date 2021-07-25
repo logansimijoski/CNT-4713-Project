0cea2b16620cc7f8e227cc26a899b23de8323a30
Logan Simijoski <logansimi@gmail.com>
2021-07-24 20:43:03 -0400

    working on heroku

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+
+from forms import SubmitForm
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'eiewgioqh95ff6nac'
+
+bootstrap = Bootstrap(app)
+
+
+@app.route("/index")
+def index():
+    return "Hello, Flask!"
+ 
+@app.route("/", methods=['GET','POST'])
+def home():
+    form = SubmitForm()
+    if request.method == 'POST' and form.validate():
+            return redirect('/submit')
+    return render_template('home.html', form = form)
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    form = SubmitForm()
+    if request.method == 'POST':
+        password = request.form['password']
+        if password == 'testing123':
+            return render_template('stream_static.html')
+        else:
+            return render_template('home.html', form = form, message = 'wrong password')
+
+
+@app.route("/stream", methods=['POST'])
+def stream():
+    return render_template('stream.html')
+
+@app.route("/static", methods=['POST'])
+def static_feed():
+    return render_template('static.html')
+
+@app.route("/stream_static", methods=['POST'])
+def stream_static():
+    return render_template('stream_static.html')
